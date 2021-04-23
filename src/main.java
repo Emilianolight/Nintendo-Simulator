7e2f8372549a16dc1452542fb25b946679d8c554
Emiliano <Emiliano@192.168.10.101>
2021-04-23 10:11:16 -0400

    commitFinal

--- src/main.java
+++ src/main.java
@@ -4,11 +4,10 @@ public class main {
     public volatile boolean seguir;
 
     public static void main(String[] args) {
-//
-//        Ventana ventana = new Ventana();
-//        ventana.setVisible(true);
-//        ventana.setLocationRelativeTo(null);
-char num ='\u0040';
-        System.out.println("num = " + num);
+
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+
     }
 }
